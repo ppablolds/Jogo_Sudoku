29893334c55c56ed628ac1a800d95fa904904b89
ppablolds <pablosilvaaluno@outlook.com>
2025-03-19 15:04:14 -0300

    feat: Func showCurrentGame, showGameStatus, clearGame and finishGame

--- src/br/com/dio/Main.java
+++ src/br/com/dio/Main.java
@@ -2,6 +2,7 @@ package br.com.dio;
 
 import br.com.dio.model.Board;
 import br.com.dio.model.Space;
+import br.com.dio.util.BoardTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -52,7 +53,7 @@ public class Main {
         }
     }
 
-    private static void startGame(Map<String, String> positions) {
+    private static void startGame(final Map<String, String> positions) {
         if (nonNull(board)) {
             System.out.println("O jogo já foi iniciado");
             return;
@@ -63,6 +64,10 @@ public class Main {
             spaces.add(new ArrayList<>());
             for (int j = 0; j < BOARD_LIMIT; j++) {
                 var positionConfig = positions.get("%s,%s".formatted(i, j));
+                if (positionConfig == null) {  // Se não houver valor na posição, usar um espaço vazio
+                    positionConfig = "0,false";  // 0 significa célula vazia e false indica que não é fixa
+                }
+
                 var expected = Integer.parseInt(positionConfig.split(",")[0]);
                 var fixed = Boolean.parseBoolean(positionConfig.split(",")[1]);
                 var currentSpace = new Space(expected, fixed);
@@ -103,23 +108,79 @@ public class Main {
         System.out.println("Informe a linha em que o número será inserido");
         var row = runUntilGetValidNumber(0, 8);
 
-        System.out.printf("Informe o número que vai entrar na posição [%s,%s]\n", col, row);
-
         if (!board.clearValue(col, row)) {
             System.out.printf("A posição [%s,%s] tem um valor fixo\n", col, row);
         }
     }
 
     private static void showCurrentGame() {
+        if (isNull(board)) {
+            System.out.println("O jogo ainda não foi iniciado");
+            return;
+        }
+
+        var args = new Object[81];
+        var argPos = 0;
+
+        for (int i = 0; i < BOARD_LIMIT; i++) {
+            for (var col: board.getSpaces()) {
+                args[argPos++] = " " + (isNull(col.get(i).getActual()) ? " " : col.get(i).getActual());
+            }
+        }
+
+        System.out.println("Seu jogo se encontra da seguinte forma");
+        System.out.printf((BoardTemplate.BOARD_TEMPLATE) + "\n", args);
     }
 
     private static void showGameStatus() {
+        if (isNull(board)) {
+            System.out.println("O jogo ainda não foi iniciado");
+            return;
+        }
+
+        System.out.printf("O seu jogo se encontra no status\n", board.getStatus().getLabel());
+
+        if (board.hasErrors()){
+            System.out.println("O jogo contém erros");
+        } else {
+            System.out.println("O jogo não contém erros");
+        }
     }
 
     private static void clearGame() {
+        if (isNull(board)) {
+            System.out.println("O jogo ainda não foi iniciado");
+            return;
+        }
+
+        System.out.println("Tem certeza que deseja limpar seu jogo e perder todo o seu progresso?");
+
+        var confirm = scanner.next();
+        while (!confirm.equalsIgnoreCase("sim") && !confirm.equalsIgnoreCase("não")) {
+            System.out.println("Informe 'sim' ou 'não'.");
+            confirm = scanner.next();
+        }
+
+        if (confirm.equalsIgnoreCase("sim")) {
+            board.reset();
+        }
     }
 
     private static void finishGame() {
+        if (isNull(board)) {
+            System.out.println("O jogo ainda não foi iniciado");
+            return;
+        }
+
+        if (board.gameIsFinished()) {
+            System.out.println("Parabéns você concluiu o jogo!");
+            showCurrentGame();
+            board = null;
+        } else if (board.hasErrors()) {
+            System.out.println("Seu jogo contém erros, verifique seu board e ajuste-o!");
+        } else {
+            System.out.println("Você ainda precisa preencher alguns espaços!");
+        }
     }
 
     private static int runUntilGetValidNumber(final int min, final int max) {
